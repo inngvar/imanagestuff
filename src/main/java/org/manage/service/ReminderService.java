6bd315a6b2b495baf6fe1c79b1ad0d2fa211d88f
mihail.b <mihail.b@sunrise-r.ru>
2026-02-17 11:47:46 +0300

    Task #4.2: implemented ReminderService

---
+++ src/main/java/org/manage/service/ReminderService.java
@@ -0,0 +1,57 @@
+package org.manage.service;
+
+import io.quarkus.scheduler.Scheduled;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.manage.client.TelegramBotClient;
+import org.manage.client.dto.SendMessageRequest;
+import org.manage.domain.Member;
+import org.manage.domain.TimeEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.List;
+
+@ApplicationScoped
+public class ReminderService {
+
+    private static final Logger log = LoggerFactory.getLogger(ReminderService.class);
+
+    @Inject
+    @RestClient
+    TelegramBotClient telegramBotClient;
+
+    @ConfigProperty(name = "telegram.bot.token")
+    String token;
+
+    @ConfigProperty(name = "reminder.message")
+    String reminderMessage;
+
+    @Scheduled(cron = "{reminder.cron}")
+    @Transactional
+    public void sendReminders() {
+        log.info("Running daily reminders task");
+        LocalDate today = LocalDate.now();
+
+        List<Member> membersWithTelegram = Member.find("telegramId IS NOT NULL").list();
+        log.info("Found {} members with Telegram linked", membersWithTelegram.size());
+
+        for (Member member : membersWithTelegram) {
+            long count = TimeEntry.count("member = ?1 and date = ?2", member, today);
+            if (count == 0) {
+                log.info("Sending reminder to member {} (telegramId: {})", member.login, member.telegramId);
+                try {
+                    telegramBotClient.sendMessage(token, new SendMessageRequest(member.telegramId, reminderMessage));
+                } catch (Exception e) {
+                    log.error("Failed to send reminder to telegramId {}: {}", member.telegramId, e.getMessage());
+                }
+            } else {
+                log.debug("Member {} already has {} time entries for today", member.login, count);
+            }
+        }
+    }
+}
